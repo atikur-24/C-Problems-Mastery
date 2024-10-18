daa92ef0cf62f3de13be61722a164efc74fed07d
atikur-24 <atikurrahman7223@gmail.com>
2024-10-18 23:42:02 +0600

    Water Distribution Optimization problems

---
+++ water_distribution_optimization.c
@@ -0,0 +1,20 @@
+Problem 1: Water Distribution Optimization
+Problem Description:
+In a small town, water is supplied to different areas through a network of pipes. The town council wants to minimize water loss by optimizing the distribution process. The town's water system is represented as a grid where each cell is a section of pipe. The amount of water that flows through each pipe is given as an integer value. Your task is to find the maximum amount of water that can be supplied from the top-left corner of the grid to the bottom-right corner by only moving right or down.
+
+Input:
+The first line contains two integers N and M representing the number of rows and columns of the grid.
+The next N lines contain M integers each, representing the amount of water that flows through each pipe in the grid.
+Output:
+A single integer representing the maximum amount of water that can flow from the top-left corner to the bottom-right corner.
+Constraints:
+2
+≤
+𝑁
+,
+𝑀
+≤
+100
+2≤N,M≤100
+Each water flow value is between 1 and 1000.
+Example:
